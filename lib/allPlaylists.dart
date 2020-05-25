6c9f769d8671a4452f8043114ca6bfd098f98431
vikas <vikasprashar99@gmail.com>
2020-05-25 16:36:55 +0530

    updated UI

--- lib/allPlaylists.dart
+++ lib/allPlaylists.dart
@@ -17,83 +17,115 @@ class PlaylistScreen extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-            Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-              child: Column(
-            children: <Widget>[
-              Container(
-                height: 130.0,
-                decoration: BoxDecoration(
-                  color: Colors.red[100],
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              SizedBox(height: 50.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "All Episode",
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w800,
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 130.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red[100],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Text("yo")
+                            ),
+                            SizedBox(width: 20.0,),
+                        Text("Bhasoodi")
+                      ],
                     ),
                   ),
-                  Text(
-                    "View All",
-                    style: TextStyle(fontSize: 22.0, color: Colors.pinkAccent),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    height: 250.0,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(10.0),
+                ),
+                SizedBox(height: 50.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "All Episode",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w800,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 250.0,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10.0),
+                    Text(
+                      "View All",
+                      style:
+                          TextStyle(fontSize: 22.0, color: Colors.pinkAccent),
+                    )
+                  ],
                 ),
-              ),
-               SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 250.0,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10.0),
+                SizedBox(
+                  height: 30.0,
                 ),
-              ), SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 250.0,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10.0),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          )),
-        ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 260.0,
+                            height: 170.0,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
-         
       ),
     );
   }
